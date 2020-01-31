04edb39b952374dd00b95373816506592b932362
emrecang <emre1907can1907@gmail.com>
2020-01-31 22:36:06 +0300

    Draw Outline

---
+++ Assets/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI partInfoText;
+    public static UIManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void ChangePartText(string text)
+    {
+        partInfoText.text = text;
+    }
+}

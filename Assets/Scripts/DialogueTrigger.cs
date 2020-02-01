0472f580594032d1563023e3ba904e73f102380e
emrecang <emre1907can1907@gmail.com>
2020-02-01 15:46:15 +0300

    Revert "Revert "Data""
    This reverts commit e31a5c7e4ab98f54014e2d6b02fe2b10f9f52399.

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+	public Dialogue dialogue;
+	public List<PersonData> person;
+	public int selected;
+	public void TriggerDialogue()
+	{
+		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);	
+	}
+	public void Start()
+	{
+		StartCoroutine(StartTrigger());
+	}
+	public IEnumerator StartTrigger()
+	{
+		yield return new WaitForSeconds(2f);
+		TriggerDialogue();
+	}
+}

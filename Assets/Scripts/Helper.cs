0fb6c9da7c472976f098c644f5e2ff3001c90f38
riwmd <kubilayege95@gmail.com>
2020-02-01 15:38:34 +0300

    up

--- Assets/Scripts/Helper.cs
+++ Assets/Scripts/Helper.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum SlotType 
+{
+    RAM,
+    CPU,
+    GPU,
+    FAN,
+    PSU
+}
 public class Helper : MonoBehaviour
 {
+    
     public static GameObject SendRay()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
